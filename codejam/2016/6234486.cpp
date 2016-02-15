cc8019dba48438b37736c7d6a57356ef0b509653
Soonbo Han <sbnet21@gmail.com>
2016-02-14 20:46:19 -0800

    Convert tabs to spaces

--- codejam/2016/6234486.cpp
+++ codejam/2016/6234486.cpp
@@ -17,68 +17,68 @@ bool adjacent_matrix[MAX_SIZE][MAX_SIZE];
 
 int getIndex(string s)
 {
-	for (int i = 0; i < v.size(); i++) // search
-		if (s == v[i])	return i;
-	v.push_back(s); // add only if not found
-	return v.size() - 1;	
+    for (int i = 0; i < v.size(); i++) // search
+        if (s == v[i])  return i;
+    v.push_back(s); // add only if not found
+    return v.size() - 1;    
 }
 
 int main()
 {
-	int T, M;
-	bool yes;
-	string s;
-	cin >> T;
+    int T, M;
+    bool yes;
+    string s;
+    cin >> T;
 
-	for (int i = 0 ; i < T; i++) {
-		yes = true;
-		v.clear();
-		cin >> M;
+    for (int i = 0 ; i < T; i++) {
+        yes = true;
+        v.clear();
+        cin >> M;
 
-		for (int j = 0; j < MAX_SIZE; j++) {
-			color[j] = -1; // not assigned
-			for (int k = 0; k < MAX_SIZE; k++)
-				adjacent_matrix[j][k] = false;
-		}
-		
-		for (int j = 0; j < M; j++) {
-			cin >> s;			
-			int idx1 = getIndex(s);
-			cin >> s;
-			int idx2 = getIndex(s);
-			
-			adjacent_matrix[idx1][idx2] = true;
-		}
-		
-		for (int j = 0; j < v.size(); j++) {
-			if (color[j] == -1) {
-				color[j] = 0;
-				q.push(j);
-			}
-			
-			while(!q.empty()) {
-				int item = q.front();
-				q.pop();
-								
-				for (int k = 0; k < v.size(); k++) {
-					if (adjacent_matrix[item][k] == true) {
-						if (color[k] == color[item]) {
-							yes = false;
-							while(!q.empty()) q.pop();
-							break;
-						} else if (color[k] == -1){
-							color[k] = 1 - color[item];
-							q.push(k);
-						} 
-					}
-				}
-				if (yes == false)
-					break;
-			}
-			if (yes == false)
-				break;
-		}
-		cout << "Case #" << (i + 1) << ": " << (yes ? "Yes" : "No") << endl;
-	}
-	return 0;
+        for (int j = 0; j < MAX_SIZE; j++) {
+            color[j] = -1; // not assigned
+            for (int k = 0; k < MAX_SIZE; k++)
+                adjacent_matrix[j][k] = false;
+        }
+        
+        for (int j = 0; j < M; j++) {
+            cin >> s;           
+            int idx1 = getIndex(s);
+            cin >> s;
+            int idx2 = getIndex(s);
+            
+            adjacent_matrix[idx1][idx2] = true;
+        }
+        
+        for (int j = 0; j < v.size(); j++) {
+            if (color[j] == -1) {
+                color[j] = 0;
+                q.push(j);
+            }
+            
+            while(!q.empty()) {
+                int item = q.front();
+                q.pop();
+                                
+                for (int k = 0; k < v.size(); k++) {
+                    if (adjacent_matrix[item][k] == true) {
+                        if (color[k] == color[item]) {
+                            yes = false;
+                            while(!q.empty()) q.pop();
+                            break;
+                        } else if (color[k] == -1){
+                            color[k] = 1 - color[item];
+                            q.push(k);
+                        } 
+                    }
+                }
+                if (yes == false)
+                    break;
+            }
+            if (yes == false)
+                break;
+        }
+        cout << "Case #" << (i + 1) << ": " << (yes ? "Yes" : "No") << endl;
+    }
+    return 0;
 }
